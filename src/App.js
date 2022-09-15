056fdb05972e5b2292d0640039d11a4bde4757bc
seonga <98681659+hongseonga@users.noreply.github.com>
2022-09-15 23:25:29 +0900

    기본검검색기능구현완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React, { useState } from "react";
 import "./App.css";
 import { Users } from "./users";
 
 function App() {
+  const [query, setQuery] = useState("");
+  // console.log(
+  //   Users.filter((user) => user.first_name.toLowerCase().includes("fe"))
+  // );
+
   return (
     <div className="App">
       <div className="input">
-        <input type="text" placeholder="Search..." className="search" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
       </div>
       <ul className="list">
-        {Users.map((user) => (
-          <li className="listItem">{user.first_name}</li>
+        {Users.filter((user) =>
+          user.first_name.toLowerCase().includes(query)
+        ).map((user) => (
+          <li key={user.id} className="listItem">
+            {user.first_name}
+          </li>
         ))}
       </ul>
     </div>
