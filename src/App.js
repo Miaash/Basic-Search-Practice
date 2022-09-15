d0a50684eee4841e9fa9fe8dd7eff332a0c7edd7
seonga <98681659+hongseonga@users.noreply.github.com>
2022-09-16 00:14:22 +0900

    기본 검색 기능 구현 완료2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Table from "./Table";
 import { Users } from "./users";
 
 function App() {
@@ -7,6 +8,19 @@ function App() {
   // console.log(
   //   Users.filter((user) => user.first_name.toLowerCase().includes("fe"))
   // );
+
+  const keys = ["first_name", "last_name", "email"];
+  // console.log(Users[0]["email"]);
+
+  const search = (data) => {
+    return data.filter((item) =>
+      // item.first_name.toLowerCase().includes(query) ||
+      // item.last_name.toLowerCase().includes(query) ||
+      // item.email.toLowerCase().includes(query)
+      // 여기서 some()메서드는 배열 안의 어떤 요소라도 주어진 판별 함수를 통과하는지 테스트 boolean 반환
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
 
   return (
     <div className="App">
@@ -17,8 +31,10 @@ function App() {
           className="search"
           onChange={(e) => setQuery(e.target.value)}
         />
+        <Table data={search(Users)} />
       </div>
-      <ul className="list">
+      {/* 기본 검색 기능 구현 */}
+      {/* <ul className="list">
         {Users.filter((user) =>
           user.first_name.toLowerCase().includes(query)
         ).map((user) => (
@@ -26,7 +42,7 @@ function App() {
             {user.first_name}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
